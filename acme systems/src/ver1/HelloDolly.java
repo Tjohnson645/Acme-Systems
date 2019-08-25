9b9df5d7fe59f28f4543aa9ac335bdd00a75e0a7
Tamikal Johnson <tamikalnjohnson@gmail.com>
2019-08-25 13:16:17 -0400

    more changes

--- acme systems/src/ver1/HelloDolly.java
+++ acme systems/src/ver1/HelloDolly.java
@@ -4,6 +4,7 @@ public class HelloDolly {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		system.out.println("Hello from eclipse");
 		system.out.println("Hello From Eclipse Again");
 	}
 
